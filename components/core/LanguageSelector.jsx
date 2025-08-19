d41df03728637d6ea4a83b4b186ddd6a353e61c1
ZaurBagishov <bagisovzaur6@gmail.com>
2025-08-19 11:53:08 -0700

    added header

---
+++ components/core/LanguageSelector.jsx
@@ -0,0 +1,83 @@
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+import { useRouter, usePathname } from "next/navigation";
+import ArrowDown from "../../public/svg/ArrowDown.svg";
+import AzeFlag from "../../public/svg/AZ.svg"
+import EnFlag from "../../public/svg/US.svg";
+import RuFlag from "../../public/svg/RU.svg";
+
+const flags = [
+  { name: "aze", flag: AzeFlag },
+  { name: "en", flag: EnFlag },
+  { name: "ru", flag: RuFlag },
+];
+
+const LanguageSelector = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  // detect current lang from pathname
+  const currentLang = pathname.split("/")[1] || "aze";
+  const currentFlag =
+    flags.find((f) => f.name === currentLang)?.flag || flags[0].flag;
+
+  const handleSelect = (lang) => {
+    const segments = pathname.split("/");
+    segments[1] = lang; // replace the first part with the new lang
+    const newPath = segments.join("/");
+    router.push(newPath);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="flex">
+      <div className="relative">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="cursor-pointer flex flex-row items-center text-[14px] text-white dark:text-[var(--main-color)] rounded-md focus:outline-none"
+        >
+          <Image
+            src={currentFlag}
+            alt={currentLang}
+            width={20}
+            height={20}
+            className="mr-[6px]"
+          />
+          {currentLang.toUpperCase()}
+          <Image
+            src={ArrowDown}
+            alt="Arrow Down"
+            width={26}
+            height={26}
+            className={`transition-transform duration-300 ${
+              isOpen ? "rotate-180" : "rotate-0"
+            }`}
+          />
+        </button>
+
+        {isOpen && (
+          <ul className="absolute z-50 bg-white text-center right-0 mt-1 border border-gray-300 rounded-md shadow-lg">
+            {flags.map((flag) => (
+              <li
+                key={flag.name}
+                onClick={() => handleSelect(flag.name)}
+                className={`${
+                  currentLang === flag.name
+                    ? "text-[var(--primary-color)]"
+                    : "text-gray-400"
+                } px-3 py-1 rounded-md cursor-pointer hover:bg-[#f3f4f6]`}
+              >
+                {flag.name.toUpperCase()}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default LanguageSelector;
